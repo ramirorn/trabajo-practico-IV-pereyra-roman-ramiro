36f36745c75bc37a04fff44c6b087637d0244da7
Ramiro <ramiroroman306@gmail.com>
2025-11-27 10:18:53 -0300

    interface.product hecho

---
+++ src/interface.product.ts
@@ -0,0 +1,11 @@
+interface Product {
+    name: string,
+    price: number
+} 
+
+let myProduct : Product = {
+    name: "Producto de Ramiro",
+    price: 5000,
+}
+
+console.log("Este es mi producto:", myProduct)
